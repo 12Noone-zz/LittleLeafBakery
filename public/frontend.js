6a5cb6a5faf518330e292846bdc5c060429008e1
Jill Noone <noone1200@gmail.com>
2015-10-22 22:18:14 -0400

    this is ridiculous.

--- public/frontend.js
+++
@@ -1,9 +0,0 @@
-document.querySelector( '#nav-toggle' ).addEventListener( 'click', function() {
-  this.classList.toggle( 'active' );
-  document.querySelector('.navbar').classList.toggle('open');
-  document.querySelector('#nav-toggle span').classList.toggle('active');
-});
-
-
-
-
